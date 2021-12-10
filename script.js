db444ba6d174c2d77cabb2d8d8a2e695ce50fdcb
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-10 19:25:46 +0300

    added edit goods and part of the adaptive

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ const updateInputMuch = (event) => {
   valueInputMuch = event.target.value;
 }
 
+const calcFunction = () => {
+  const sum = allGoods.reduce((accum, item)=> { 
+    return accum += Number(item.howMuch);
+  }, 0);
+  return sum;
+}
+
 const render = () => {
   const content = document.querySelector('#content-page');
   const sum = document.querySelector('.sum');
@@ -41,10 +48,24 @@ const render = () => {
 
     const { where, howMuch, day } = item;
 
+    const cancelButton = document.createElement('button');
+    cancelButton.innerText = 'Отмена';
+    cancelButton.className = 'hide';
+    container.appendChild(cancelButton);
+
+    const editContainer = document.createElement('div');
+    editContainer.className = 'hide';
+    container.appendChild(editContainer);
+
     const shop = document.createElement('p');
     shop.innerText = `${ index + 1 }) ${ where }`;
     shop.className = 'shop';
     container.appendChild(shop);
+
+    const inputEditShop = document.createElement('input');
+    inputEditShop.value = where;
+    inputEditShop.className = 'hide';
+    editContainer.appendChild(inputEditShop);
 
     const rightBlock = document.createElement('div');
     rightBlock.className = 'rightBlock';
@@ -55,10 +76,20 @@ const render = () => {
     date.className = 'date';
     rightBlock.appendChild(date);
 
+    const calendar = document.createElement('input');
+    calendar.type = 'date';
+    calendar.className = 'hide';
+    editContainer.appendChild(calendar);
+
     const cost = document.createElement('p');
     cost.innerText = `${ howMuch } р.`;
     cost.className = 'cost';
     rightBlock.appendChild(cost);
+
+    const inputEditCost = document.createElement('input');
+    inputEditCost.value = howMuch;
+    inputEditCost.className = 'hide';
+    editContainer.appendChild(inputEditCost);
 
     const icons = document.createElement('div');
     icons.className = 'icons';
@@ -75,10 +106,110 @@ const render = () => {
     icons.appendChild(deleteImage);
     content.appendChild(container);
 
+    const buttonSave = document.createElement('button');
+    buttonSave.innerText = 'Сохранить';
+    buttonSave.className = 'hide';
+    container.appendChild(buttonSave);
+
     deleteImage.onclick = () => {
-      removeGood(container);
+      removeTask(container);
+    }
+
+    editImage.onclick = () => {
+      editFunction(container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave, editContainer);
+    }
+
+    cancelButton.onclick = () => {
+      cancelEdit(container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave);
+    }
+
+    buttonSave.onclick = () => {
+      saveChanges(container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave, index);
     }
   });
+}
+
+const removeTask = (collection) => {
+  allGoods = allGoods.filter((item, index) => index !== +collection.id);
+  render();
+}
+
+const saveChanges = (container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave, index) => {
+  allGoods[index].where = inputEditShop.value;
+  allGoods[index].howMuch = inputEditCost.value;
+
+  let newStr = calendar.value.replace(/-/gi, '.');
+  let newArray = newStr.split('.').reverse();
+  let str = '';
+  newArray.forEach(item => {
+    if(item.length !== 4) {
+      str += `${item}.`
+    } else {
+      str += item;
+    }
+  })
+  
+  allGoods[index].day = str;
+
+  container.classList.remove('editContainer');
+  cancelButton.classList.add('hide');
+  shop.classList.remove('editShop');
+  inputEditShop.classList.remove('inputEditShop');
+  inputEditShop.classList.add('hide');
+  rightBlock.classList.remove('editRightBlock')
+  date.classList.remove('editDate');
+  calendar.classList.add('hide');
+  cost.classList.remove('editCost');
+  inputEditCost.classList.add('hide');
+  inputEditCost.classList.remove('inputEditCost');
+  icons.classList.remove('hide');
+  buttonSave.classList.add('hide');
+  calcFunction();
+  render();
+}
+
+const editFunction = (container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave, editContainer) => {
+  container.classList.add('editContainer');
+  cancelButton.classList.remove('hide');
+  cancelButton.classList.add('cancelButton'); 
+  shop.classList.add('editShop');
+  shop.classList.add('hide');
+  inputEditShop.classList.remove('hide');
+  inputEditShop.classList.add('inputEditShop');
+  rightBlock.classList.add('editRightBlock');
+  date.classList.add('editDate');
+  date.classList.add('hide');
+  calendar.classList.remove('hide');
+  calendar.classList.add('calendar');
+  cost.classList.add('editCost');
+  cost.classList.add('hide');
+  inputEditCost.classList.remove('hide');
+  inputEditCost.classList.add('inputEditCost');
+  icons.classList.remove('icons');
+  icons.classList.add('hide');
+  buttonSave.classList.remove('hide');
+  buttonSave.classList.add('saveButton');
+  editContainer.classList.remove('hide');
+  editContainer.classList.add('for-inputs');
+}
+
+const cancelEdit = (container, cancelButton, shop, inputEditShop, rightBlock, date, calendar, cost, inputEditCost, icons, buttonSave) => {
+  container.classList.remove('editContainer');
+  cancelButton.classList.add('hide');
+  shop.classList.remove('editShop');
+  shop.classList.remove('hide');
+  inputEditShop.classList.remove('inputEditShop');
+  inputEditShop.classList.add('hide');
+  rightBlock.classList.remove('editRightBlock')
+  date.classList.remove('editDate');
+  date.classList.remove('hide');
+  calendar.classList.add('hide');
+  cost.classList.remove('editCost');
+  cost.classList.remove('hide');
+  inputEditCost.classList.add('hide');
+  inputEditCost.classList.remove('inputEditCost');
+  icons.classList.remove('hide');
+  buttonSave.classList.add('hide');
 }
 
 const getDate = () => {
@@ -88,13 +219,6 @@ const getDate = () => {
   const yyyy = today.getFullYear();
   today = `${dd}.${mm}.${yyyy}`;
   return today;
-}
-
-const calcFunction = () => {
-  const sum = allGoods.reduce((accum, item)=> { 
-    return accum += Number(item.howMuch);
-  }, 0);
-  return sum;
 }
 
 const onClickButton = () => {
@@ -113,9 +237,4 @@ const onClickButton = () => {
   render();
 }
 
-const removeGood = (collection) => {
-  allGoods = allGoods.filter((item, index) => index !== Number(collection.id));
-  render();
-}
-
-document.querySelector('.add-btn').addEventListener('click', onClickButton)
+document.querySelector('.add-btn').addEventListener('click', onClickButton);
